edcccc8e68bdb9bec317c206b0235576131a06d4
William Edmisten <wce@vt.edu>
2021-12-23 11:12:04 -0500

    Add colored cities

--- catan.py
+++ catan.py
@@ -33,48 +33,48 @@ totals = {
 item_prices = {
     'road': [
     {
-        'resource': 'wood',
+        'resource': R.WOOD,
         'quanitity': 1
     },
     {
-        'resource': 'brick',
+        'resource': R.BRICK,
         'quanitity': 1
     }],
     'settlement': [
         {
-        'resource': 'wood',
+        'resource': R.WOOD,
         'quanitity': 1
     },
     {
-        'resource': 'brick',
+        'resource': R.BRICK,
         'quanitity': 1
     },
     {
-        'resource': 'wheat',
+        'resource': R.HAY,
         'quanitity': 1
     },
     {
-        'resource': 'sheep',
+        'resource': R.SHEEP,
         'quanitity': 1
     }],
     'city': [{
-        'resource': 'wheat',
+        'resource': R.HAY,
         'quanitity': 2
     },
     {
-        'resource': 'stone',
+        'resource': R.ROCK,
         'quanitity': 3
     }],
     'development': [{
-        'resource': 'wheat',
+        'resource': R.HAY,
         'quanitity': 1
     },
     {
-        'resource': 'sheep',
+        'resource': R.SHEEP,
         'quanitity': 1
     },
     {
-        'resource': 'rock',
+        'resource': R.ROCK,
         'quanitity': 1
     }]
 }
@@ -145,61 +145,71 @@ first, third, and fifth rows count off by 2s
 
 spaces = [
     [None, None, None, None, None, None, None],
-    [None, None, None, None, None, None, None, None, None],
+    [None, None, '1@', None, None, None, None, '4@', None],
     [None, None, None, None, None, None, None, None, None, None, None],
-    [None, None, None, None, None, None, None, None, None, None, None],
-    [None, None, None, None, None, None, None, None, None],
+    [None, None, None, '2@', None, None, None, None, None, None, None],
+    [None, None, None, None, None, None, None, '3@', None],
     [None, None, None, None, None, None, None],
 ]
+
+# roads are marked as between two spaces, e.g.
+# row1, col1, row2, col2
+(0, 1, 0, 2)
+
+player_colors = color = {
+    '1': Fore.RED,
+    '2': Fore.CYAN,
+    '3': Fore.MAGENTA,
+    '4': Fore.YELLOW,
+}
 
 def spce(r, c):
     if spaces[r][c] is None:
         return " "
-    return spaces[r][c]
+    return player_colors[spaces[r][c][0]] + spaces[r][c][1] + Style.RESET_ALL
 
 board_string = rf"""
-
                   /{spce(0,1)}\         /{spce(0,3)}\         /{spce(0,5)}\
                 /     \     /     \     /     \
              {spce(0,0)}/         \{spce(0,2)}/         \{spce(0,4)}/         \{spce(0,6)}
-             |           |                       |
+             |           |           |           |
              |    {tile(0, 0)}    |    {tile(0, 1)}    |    {tile(0, 2)}    |
              |    {dots(0, 0)}    |    {dots(0, 1)}    |    {dots(0, 2)}    |
-            / \         / \         / \         / \
+            /{spce(1,1)}\         /{spce(1,3)}\         /{spce(1,5)}\         /{spce(1,7)}\
           /     \     /     \     /     \     /     \
-        /         \ /         \ /         \ /         \
+       {spce(1,0)}/         \{spce(1,2)}/         \{spce(1,4)}/         \{spce(1,6)}/         \{spce(1,8)}
        |           |           |           |           |
        |    {tile(1, 0)}    |    {tile(1, 1)}    |    {tile(1, 2)}    |    {tile(1, 3)}    |
        |    {dots(1, 0)}    |    {dots(1, 1)}    |    {dots(1, 2)}    |    {dots(1, 3)}    |
-      / \         / \         / \         / \         / \
+      /{spce(2,1)}\         /{spce(2,3)}\         /{spce(2,5)}\         /{spce(2,7)}\         /{spce(2,9)}\
     /     \     /     \     /     \     /     \     /     \
-  /         \ /         \ /         \ /         \ /         \
+ {spce(2,0)}/         \{spce(2,2)}/         \{spce(2,4)}/         \{spce(2,6)}/         \{spce(2,8)}/         \{spce(2,10)}
  |           |           |           |           |           |
  |    {tile(2, 0)}    |    {tile(2, 1)}    |    {tile(2, 2)}    |    {tile(2, 3)}    |    {tile(2, 4)}    |
  |    {dots(2, 0)}    |    {dots(2, 1)}    |    {dots(2, 2)}    |    {dots(2, 3)}    |    {dots(2, 4)}    |
-  \         / \         / \         / \         / \         /
+ {spce(3,0)}\         /{spce(3,2)}\         /{spce(3,4)}\         /{spce(3,6)}\         /{spce(3,8)}\         /{spce(3,10)}
     \     /     \     /     \     /     \     /     \     / 
-      \ /         \ /         \ /         \ /         \ /
+      \{spce(3,1)}/         \{spce(3,3)}/         \{spce(3,5)}/         \{spce(3,7)}/         \{spce(3,9)}/
        |           |           |           |           |
        |    {tile(3, 0)}    |    {tile(3, 1)}    |    {tile(3, 2)}    |    {tile(3, 3)}    |
        |    {dots(3, 0)}    |    {dots(3, 1)}    |    {dots(3, 2)}    |    {dots(3, 3)}    |
-        \         / \         / \         / \         /
+       {spce(4,0)}\         /{spce(4,2)}\         /{spce(4,4)}\         /{spce(4,6)}\         /{spce(4,8)}
           \     /     \     /     \     /     \     /
-            \ /         \ /         \ /         \ /
+            \{spce(4,1)}/         \{spce(4,3)}/         \{spce(4,5)}/         \{spce(4,7)}/
              |           |           |           |
              |    {tile(4, 0)}    |    {tile(4, 1)}    |    {tile(4, 2)}    |
              |    {dots(4, 0)}    |    {dots(4, 1)}    |    {dots(4, 2)}    |
-              \         / \         / \         /
+             {spce(5,0)}\         /{spce(5,2)}\         /{spce(5,4)}\         /{spce(5,6)}
                 \     /     \     /     \     /
-                  \ /         \ /         \ /
+                  \{spce(5,1)}/         \{spce(5,3)}/         \{spce(5,5)}/
 """
 
 print(board_string)
 
 space_tile_lookup = [
-    [[(0,0)], [(0,0)], [(0,0),(0,1)], [(0,1)], [(0,1), (0,2)], [(0,2)], [(0,2)]],
-    [[(1,0)], [(1,0), (0,0)], [(0,0),(1,0),(1,1)], [(0, 0), (1,1), (0,1)], [(1,1), (0,1), (1,2)], [(0,1),(1,2),(0,2)], [(1,2), (0,2),(1,3)], [(0,2),(1,3)], [(1,3)]],
-    [[(2,0)], [(2,0), (1,0)], [(1,0), (2,0), (2,1)], [(2,0), (2,1), (1,1)], [(2,1), (2,2), (1,1)], [(1,1),(1,2),(2,2)], [(2,2),(2,3),(1,2)], [(1,2),(1,3),(2,3)], [(2,3),(2,4),(1,3)], [(1,3),(2,4)], [(2,4)]],
+    [[(0,0)], [(0,0)], [(0,0),(0,1)], [(0,1)], [(0,1),(0,2)], [(0,2)], [(0,2)]],
+    [[(1,0)], [(1,0),(0,0)], [(0,0),(1,0),(1,1)], [(0,0),(1,1),(0,1)], [(1,1),(0,1),(1,2)], [(0,1),(1,2),(0,2)], [(1,2),(0,2),(1,3)], [(0,2),(1,3)], [(1,3)]],
+    [[(2,0)], [(2,0),(1,0)], [(1,0),(2,0),(2,1)], [(2,0),(2,1),(1,1)], [(2,1),(2,2),(1,1)], [(1,1),(1,2),(2,2)], [(2,2),(2,3),(1,2)], [(1,2),(1,3),(2,3)], [(2,3),(2,4),(1,3)], [(1,3),(2,4)], [(2,4)]],
     [[(2,0)], [(2,0),(3,0)], [(2,0),(3,0),(2,1)], [(3,0),(3,1),(2,1)], [(2,1),(2,2),(3,1)], [(3,1),(3,2),(2,2)], [(2,2),(2,3),(3,2)], [(3,2),(3,3),(2,3)], [(2,3),(2,4),(3,3)], [(2,4),(3,3)], [(2,4)]],
     [[(3,0)], [(3,0),(4,0)], [(3,0),(4,0),(3,1)], [(4,0),(4,1),(3,1)], [(3,1),(3,2),(4,1)], [(4,1),(4,2),(3,2)], [(3,2),(3,3),(4,2)], [(3,3),(4,2)], [(3,3)]],
     [[(4,0)], [(4,0)], [(4,0),(4,1)], [(4,1)], [(4,1),(4,2)], [(4,2)], [(4,2)]],
@@ -216,10 +226,9 @@ for r, row in enumerate(spaces):
         for tile in tiles:
             r1, c1 = tile
             total += num_dots[board[r1][c1][1]]
-        # print(r, c, total)
         if total not in score_map:
             score_map[total] = []
 
         score_map[total].append((r, c))
 
-pprint.pprint(score_map)
+# pprint.pprint(score_map)
